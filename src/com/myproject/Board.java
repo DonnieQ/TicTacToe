66130698cb7a3a55833e6902d5147fd73cbbb3f8
matt <mjanousek23@gmail.com>
2020-11-08 16:42:07 -0800

    Added Board changes (added the Boolean swap for isComputer, changed button color, adjusted how the buttons are placed on the JPanel)

--- src/com/myproject/Board.java
+++ src/com/myproject/Board.java
@@ -4,72 +4,68 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class Board implements ActionListener {
+public class Board {
     JFrame board = new JFrame("Tic-Tac-Toe");
-    JButton [] tile = new JButton[9];
+    JButton[] tile = new JButton[9];
     JPanel cellPanel = new JPanel();
     JPanel titlePanel = new JPanel();
     JLabel titleText = new JLabel();
-    boolean isComputer;
-    public Board (){
+    public static boolean isComputer;
+    JOptionPane popup = new JOptionPane();
+    List<JButton> tiles = new ArrayList<>();
+
+    public Board() {
+
         board.setVisible(true);
-        board.setSize(900,900);
+        board.setSize(900, 900);
         board.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         board.setLayout(new BorderLayout());
+        board.getContentPane();
 
         titlePanel.setLayout(new BorderLayout());
-        titlePanel.setBounds(0,0,100,500);
+        titlePanel.setBounds(0, 0, 100, 500);
 
         titleText.setHorizontalAlignment(JLabel.RIGHT);
         titleText.setText("Team 3 Project");
         titleText.setOpaque(false);
 
-        cellPanel.setLayout(new GridLayout(3,3));
+        cellPanel.setLayout(new GridLayout(3, 3));
         cellPanel.setBackground(Color.BLACK);
-
-        tile[0] = new JButton("1");
-        tile[1] = new JButton("2");
-        tile[2] = new JButton("3");
-        tile[3] = new JButton("4");
-        tile[4] = new JButton("5");
-        tile[5] = new JButton("6");
-        tile[6] = new JButton("7");
-        tile[7] = new JButton("8");
-        tile[8] = new JButton("9");
-
-        tile[0].addActionListener(this::actionPerformed);
-        tile[1].addActionListener(this::actionPerformed);
-        tile[2].addActionListener(this::actionPerformed);
-        tile[3].addActionListener(this::actionPerformed);
-        tile[4].addActionListener(this::actionPerformed);
-        tile[5].addActionListener(this::actionPerformed);
-        tile[6].addActionListener(this::actionPerformed);
-        tile[7].addActionListener(this::actionPerformed);
-        tile[8].addActionListener(this::actionPerformed);
-
-        cellPanel.add(tile[0]);
-        cellPanel.add(tile[1]);
-        cellPanel.add(tile[2]);
-        cellPanel.add(tile[3]);
-        cellPanel.add(tile[4]);
-        cellPanel.add(tile[5]);
-        cellPanel.add(tile[6]);
-        cellPanel.add(tile[7]);
-        cellPanel.add(tile[8]);
 
         titlePanel.add(titleText);
         board.add(cellPanel);
         board.add(titlePanel, BorderLayout.SOUTH);
 
+        for (int i = 0; i < 9; i++) {
+            tile[i] = new JButton();
+            tile[i].setBackground(Color.BLACK);
+            cellPanel.add(tile[i]);
+            tiles.add(tile[i]);
+            tile[i].addActionListener(this::actionPerformed);
+            tile[i].setText("Click Me!");
+        }
     }
 
-    @Override
     public void actionPerformed(ActionEvent e) {
-        if(isComputer) {
-            tile[0].setIcon(new ImageIcon("halloween_pennywise-512.png"));
-        }if(isComputer = false);
-        tile[0].setIcon(new ImageIcon("halloween_pennywise-512.png"));
+        JButton selected = (JButton) e.getSource();
+        if (isComputer && selected.getText().equals("Click Me!")) {
+            selected.setIcon(new ImageIcon("halloween_pennywise-512.png"));
+            selected.setText("");
+            isComputer = false;
+            //TODO send to list to count wins?
+        } else if ((!isComputer && selected.getText().equals("Click Me!"))) {
+            selected.setIcon(new ImageIcon("yellowRaincoat.png"));
+            selected.setText("");
+            isComputer = true;
+        } else if (!selected.getText().equals("Click Me!")) {
+            System.out.println("Spot taken");
 
+            //TODO Figure out how to get a message if spot is taken
+        }
     }
 }
+
