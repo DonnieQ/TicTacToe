932891b67b6a1927deba604693013731325e9a4f
matt <mjanousek23@gmail.com>
2020-11-09 08:42:23 -0800

    Added Board changes (added start())

--- src/com/myproject/Board.java
+++ src/com/myproject/Board.java
@@ -4,19 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.sql.PseudoColumnUsage;
+import java.util.*;
 import java.util.List;
 
 public class Board {
-    JFrame board = new JFrame("Tic-Tac-Toe");
-    JButton[] tile = new JButton[9];
-    JPanel cellPanel = new JPanel();
-    JPanel titlePanel = new JPanel();
-    JLabel titleText = new JLabel();
-    public static boolean isComputer;
+    private JFrame board = new JFrame("Tic-Tac-Toe");
+    private JButton[] tile = new JButton[9];
+    private JPanel cellPanel = new JPanel();
+    private JPanel titlePanel = new JPanel();
+    private JLabel titleText = new JLabel();
+    private boolean isComputer = true;
     JOptionPane popup = new JOptionPane();
     List<JButton> tiles = new ArrayList<>();
+
+    Collection<JButton> compWins = new ArrayList<>();
+    Collection<JButton> humanWins = new ArrayList<>();
 
     public Board() {
 
@@ -36,6 +39,35 @@ public class Board {
         cellPanel.setLayout(new GridLayout(3, 3));
         cellPanel.setBackground(Color.BLACK);
 
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        JButton selected = (JButton) e.getSource();
+        if (isComputer && selected.getText().equals("Click Me!")) {
+            selected.setIcon(new ImageIcon("halloween_pennywise-512.png"));
+            selected.setText("");
+            compWins.add(selected);
+            isComputer = false;
+            System.out.println(selected.getActionCommand());
+
+
+            //TODO send to list to count wins?
+        } else if ((!isComputer && selected.getText().equals("Click Me!"))) {
+            selected.setIcon(new ImageIcon("yellowRaincoat.png"));
+            selected.setText("");
+            humanWins.add(selected);
+            isComputer = true;
+            System.out.println(selected);
+
+        } else if (!selected.getText().equals("Click Me!")) {
+            System.out.println("Spot taken");
+
+            //TODO Figure out how to get a message if spot is taken
+        }
+    }
+
+    public void start() {
         titlePanel.add(titleText);
         board.add(cellPanel);
         board.add(titlePanel, BorderLayout.SOUTH);
@@ -45,27 +77,14 @@ public class Board {
             tile[i].setBackground(Color.BLACK);
             cellPanel.add(tile[i]);
             tiles.add(tile[i]);
+            tile[i].setActionCommand("this is tile" + i);
             tile[i].addActionListener(this::actionPerformed);
             tile[i].setText("Click Me!");
+
         }
     }
 
-    public void actionPerformed(ActionEvent e) {
-        JButton selected = (JButton) e.getSource();
-        if (isComputer && selected.getText().equals("Click Me!")) {
-            selected.setIcon(new ImageIcon("halloween_pennywise-512.png"));
-            selected.setText("");
-            isComputer = false;
-            //TODO send to list to count wins?
-        } else if ((!isComputer && selected.getText().equals("Click Me!"))) {
-            selected.setIcon(new ImageIcon("yellowRaincoat.png"));
-            selected.setText("");
-            isComputer = true;
-        } else if (!selected.getText().equals("Click Me!")) {
-            System.out.println("Spot taken");
 
-            //TODO Figure out how to get a message if spot is taken
-        }
-    }
 }
+
 
