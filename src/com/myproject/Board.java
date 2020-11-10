17843dacfb28d6974c7e3948b0eef613927de5a3
matt <mjanousek23@gmail.com>
2020-11-10 10:04:33 -0800

    Added Board changes(refactored names, added to computerTurn)

--- src/com/myproject/Board.java
+++ src/com/myproject/Board.java
@@ -9,39 +9,46 @@ import java.util.*;
 import java.util.List;
 
 public class Board {
-    private JFrame board = new JFrame("Tic-Tac-Toe");
+    private JFrame board;
     private JButton[] tile = new JButton[10];
-    private JPanel cellPanel = new JPanel();
-    private JPanel titlePanel = new JPanel();
-    private JLabel titleText = new JLabel();
+    private JPanel cellPanel;
+    private JPanel titlePanel;
+    private JLabel titleText;
     private boolean isComputer;
-    JOptionPane popup = new JOptionPane();
-    List<JButton> tiles = new ArrayList<>();
-    Map<String, String>coolMap = new HashMap<>();
 
-    Collection<JButton> compWins = new ArrayList<>();
-    Collection<JButton> humanWins = new ArrayList<>();
+    List<JButton> tiles = new ArrayList<>();
+    Collection<JButton> compSelection = new ArrayList<>();
+    Collection<JButton> humanSelection = new ArrayList<>();
+
+    Thread thread = new Thread();
 
     public Board() {
+        board = new JFrame("Tic-Tac-Toe");
 
         board.setVisible(true);
         board.setSize(900, 900);
         board.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        board.setLayout(new BorderLayout());
+        board.setLayout(new BorderLayout(15, 15));
         board.getContentPane();
 
-        titlePanel.setLayout(new BorderLayout());
-        titlePanel.setBounds(0, 0, 100, 500);
+        titlePanel = new JPanel();
+
+        titlePanel.setLayout(new BorderLayout(15, 15));
+        titlePanel.setBounds(0, 0, 0, 0);
+
+        titleText = new JLabel();
 
         titleText.setHorizontalAlignment(JLabel.RIGHT);
-        titleText.setText("Team 3 Project");
-        titleText.setOpaque(false);
+        titleText.setText("Team 3 Project ");
+        // titleText.setOpaque(false);
+
+        cellPanel = new JPanel();
 
         cellPanel.setLayout(new GridLayout(3, 3));
         cellPanel.setBackground(Color.BLACK);
-
     }
 
+    //TODO Separate ActionListener
 
     public void actionPerformed(ActionEvent e) {
         JButton selected = (JButton) e.getSource();
@@ -49,9 +56,9 @@ public class Board {
             selected.setIcon(new ImageIcon("halloween_pennywise-512.png"));
             selected.setText("");
             tiles.add(selected);
+            compSelection.add(selected);
 
             isComputer = false;
-
 
 
             //TODO send to list to count wins?
@@ -59,9 +66,13 @@ public class Board {
             selected.setIcon(new ImageIcon("yellowRaincoat.png"));
             selected.setText("");
             tiles.add(selected);
-            isComputer = true;
-            computerTurn();
+            humanSelection.add(selected);
 
+            isComputer = true;
+            //TODO
+            computerTurn();
+            checkWinner();
+            System.out.println(humanSelection);
         }
     }
 
@@ -74,29 +85,177 @@ public class Board {
             tile[i] = new JButton();
             tile[i].setBackground(Color.BLACK);
             cellPanel.add(tile[i]);
-        //    tiles.add(tile[i]);
-            tile[i].setActionCommand("" + i);
+            //    tiles.add(tile[i]);
+           // tile[i].setActionCommand(String.valueOf(i));
             tile[i].addActionListener(this::actionPerformed);
             tile[i].setText("Click Me!");
 
         }
     }
 
+    //TODO map?
+    
     public void computerTurn() {
-        ComputerBrain brain = new ComputerBrain();
-        int rand = brain.think();
-        JButton select = (JButton) tile[rand];
-        if (select.getText().equals("Click Me!")) {
-            select.doClick();
-        } else if (tiles.size() == 9){
-            System.out.println("You win!");
-        }else {
-            computerTurn();
-        }
+
+        while (isComputer && tiles.size() <= 9) {
+            int rand = ComputerBrain.think();
+            JButton select = tile[rand];
+            if (tile[5].getText().equals("Click Me!")) {
+                tile[5].doClick();
+            } else if (tile[3].getText().equals("Click Me!")) {
+                tile[3].doClick();
+            } else if (tile[7].getText().equals("Click Me!")) {
+                tile[7].doClick();
+            } else if (tile[9].getText().equals("Click Me!")) {
+                tile[9].doClick();
+            } else if (tile[1].getText().equals("Click Me!")) {
+                tile[1].doClick();
+            } else if (tile[4].getText().equals("Click Me!")) {
+                tile[4].doClick();
+            } else if (tile[6].getText().equals("Click Me!")) {
+                tile[6].doClick();
+            } else if (select.getText().equals("Click Me!")) {
+                select.doClick();
+            } else {
+                break;
+            }
 
         }
 
     }
+
+
+    public void checkWinner() {
+
+        for (JButton i : humanSelection) {
+            if (humanSelection.contains(tile[1]) &&
+                    humanSelection.contains(tile[2]) &&
+                    humanSelection.contains(tile[3])) {
+                System.out.println("checkWinner");
+
+            }
+        }
+    }
+}
+
+//    public void hardMode() {
+//        JButton select = new JButton();
+//        ComputerBrain brain = new ComputerBrain();
+//        int rand = brain.think();
+//        JButton selectRand = (JButton) tile[rand];
+//
+//        try {
+//            thread.sleep(800);
+//            if (humanSelection.contains(tile[1]) &&
+//                    humanSelection.contains(tile[2])) {
+//                select = tile[3];
+//
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            thread.sleep(800);
+//            if (humanSelection.contains(tile[2]) &&
+//                    humanSelection.contains(tile[3])) {
+//                select = tile[1];
+//
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            thread.sleep(800);
+//            if (humanSelection.contains(tile[3]) &&
+//                    humanSelection.contains(tile[1])) {
+//                select = tile[2];
+//
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            thread.sleep(800);
+//            if (humanSelection.contains(tile[4]) &&
+//                    humanSelection.contains(tile[5])) {
+//                select = tile[6];
+//
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            thread.sleep(800);
+//            if (humanSelection.contains(tile[6]) &&
+//                    humanSelection.contains(tile[4])) {
+//                select = tile[5];
+//
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            thread.sleep(800);
+//            if (humanSelection.contains(tile[5]) &&
+//                    humanSelection.contains(tile[6])) {
+//                select = tile[4];
+//
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            thread.sleep(800);
+//            if (humanSelection.contains(tile[7]) &&
+//                    humanSelection.contains(tile[8])) {
+//                select = tile[9];
+//
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            thread.sleep(800);
+//            if (humanSelection.contains(tile[9]) &&
+//                    humanSelection.contains(tile[7])) {
+//                select = tile[8];
+//
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            if (humanSelection.contains(tile[8]) &&
+//                    humanSelection.contains(tile[9])) {
+//                select = tile[7];
+//                thread.sleep(800);
+//                select.doClick(5);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/com/myproject/ComputerBrain.java
+++ src/com/myproject/ComputerBrain.java
@@ -2,11 +2,13 @@ package com.myproject;
 
 import java.util.Random;
 
-public class ComputerBrain {
-    public int think() {
-        int min =1;
-        int max =9;
-        int random_int = (int)(Math.random() * (max-min+1) + min);  //generate random values from 1 to 9
+class ComputerBrain {
+
+
+    public static int think() {
+        int min = 1;
+        int max = 9;
+        int random_int = (int) (Math.random() * (max - min + 1) + min);  //generate random values from 1 to 9
         return random_int;
     }
 }
